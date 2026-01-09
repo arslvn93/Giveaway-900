ebc6754880815ade51e332c6dcd3593c09d52e68
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2026-01-09 16:48:36 -0500

    Update Site

--- config.js
+++ config.js
@@ -24,7 +24,7 @@ const config = {
       "#FFFFFF"
     ],
     "showCountdownInHero": true,
-    "theme": "dark",
+    "theme": "light",
     "primaryColorOverride": "",
     "secondaryColorOverride": "",
     "ghlWebhookUrl": "https://services.leadconnectorhq.com/hooks/bJbBMjcW619cJrLUJ3kY/webhook-trigger/3yNsurfrt9oBUtRBuygb",
@@ -185,7 +185,7 @@ const config = {
   },
   "modalQuestions": [
     {
-      "id": "q_auto_1767909477328_0",
+      "id": "q_auto_1767995314635_0",
       "questionText": "What's your current situation?",
       "options": [
         {
@@ -207,7 +207,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1767909477328_1",
+      "id": "q_auto_1767995314635_1",
       "questionText": "What are you looking to do?",
       "options": [
         {
@@ -229,7 +229,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1767909477328_2",
+      "id": "q_auto_1767995314635_2",
       "questionText": "When are you planning to move?",
       "options": [
         {
