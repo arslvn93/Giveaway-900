7605a5655b147f896309e31245374bc08b2a08ae
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2026-01-14 11:21:51 -0500

    Update Site

--- config.js
+++ config.js
@@ -185,7 +185,7 @@ const config = {
   },
   "modalQuestions": [
     {
-      "id": "q_auto_1768406347564_0",
+      "id": "q_auto_1768407710278_0",
       "questionText": "What's your current situation?",
       "options": [
         {
@@ -207,7 +207,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1768406347564_1",
+      "id": "q_auto_1768407710278_1",
       "questionText": "What are you looking to do?",
       "options": [
         {
@@ -229,7 +229,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1768406347564_2",
+      "id": "q_auto_1768407710278_2",
       "questionText": "When are you planning to move?",
       "options": [
         {
